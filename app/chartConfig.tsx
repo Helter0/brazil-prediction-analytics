28b8ffd75eeede8cd4bfa65f4f125638b4e8a23e
Helter0 <159495440+Helter0@users.noreply.github.com>
2025-10-25 17:53:25 +0300

    Initialize chart configuration wAdd app/chartConfig.tsxith Chart.js setup

---
+++ app/chartConfig.tsx
@@ -0,0 +1,4 @@
+"use client";
+import { Chart, BarElement, CategoryScale, LinearScale, Legend, Tooltip, ArcElement } from 'chart.js';
+
+Chart.register(BarElement, CategoryScale, LinearScale, Legend, Tooltip, ArcElement);
